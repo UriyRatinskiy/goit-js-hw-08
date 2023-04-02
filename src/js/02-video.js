996e4e9fe6ccf2e6e74f7ccf5fd67d214654fce2
UriyRatinskiy <117594065+UriyRatinskiy@users.noreply.github.com>
2023-04-02 17:17:23 +0300

    hw 08 03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+  
+    player.on('timeupdate', throttle (onUpdatingTime, 1000));
+
+    function onUpdatingTime (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+    }
+
+    const videoplayerCurrentTime = localStorage.getItem("videoplayer-current-time");
+
+    player.setCurrentTime(videoplayerCurrentTime);
