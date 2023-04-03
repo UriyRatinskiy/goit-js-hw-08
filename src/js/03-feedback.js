511b282dcaa879458c18192ccbbfe7b8b6d49db7
UriyRatinskiy <117594065+UriyRatinskiy@users.noreply.github.com>
2023-04-03 17:26:16 +0300

    hw 08 05

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "feedback-form-state";
+
+const formData = {};
+
+const refs = {
+    form: document.querySelector('form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+};
+
+populateFormData()
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput (event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateFormData() {
+    const savedInformation = localStorage.getItem(STORAGE_KEY);
+
+    if (savedInformation) {
+        const parsedInformation = JSON.parse(savedInformation);
+        refs.input.value = parsedInformation.email;
+        refs.textarea.value = parsedInformation.message;
+        return parsedInformation;
+    }
+}
